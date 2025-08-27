a2658463d169549bd6e9d493dc91262cef9c8cfe
AkshayGowda-s <akshayshivaramu@gmail.com>
2025-08-27 17:43:35 +0530

    changes in  ui

--- gs-application/Grocery application/backend/sql_connection.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-__cnx = None
-def get_sql_connection():
-    global __cnx
-    if __cnx is None:
-        __cnx = mysql.connector.connect(user='root', password='Akshay@06',
-                                        host='localhost',
-                                        database='gs')
-    return __cnx
